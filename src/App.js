02076cc4bbe12a236de48fdf2554bf2eaed7f146
Vincent K <vincentkuan91@gmail.com>
2020-07-29 14:36:47 +1200

    react checklist 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+        notes: [
+            {
+              id:1,
+              text: 'Study'
+            },
+            {
+              id:2,
+              text: 'Eat'
+            },
+            {
+              id:3,
+              text: 'Drink'
+            },
+            {
+              id:4,
+              text: 'Sleep'
+            },
+            {
+              id:5,
+              text: 'Exercise'
+            },
+        ],
+        noteInputValue:''
+    }
+    
+  }
+  render(){
+    return (
+     
+    <div class="wrap">
+    <div class="container">
+    <div class="notes">
+
+        {
+            this.state.notes.map((note)=>{
+
+                return (
+
+                    <div class="note">
+                        <div class="note-body">
+                            <i class="fa fa-times note-remove" aria-hidden="remove"></i>
+                            <i class="fa fa-plus note-plus" aria-hidden="plus"></i>
+                            <div class="note-text">
+                                {note.text}
+                            </div>
+                        </div>
+                    </div>
+
+
+                )
+            })
+        }
+
+           
+           
+
+            <div class="note new-note">
+
+					<form class="note-body">
+					  	<div class="form-group">
+					    	<label for="note-input">New note</label>
+					    	<input type="text" class="form-control" id="note-input"/>
+					  	</div>
+				
+					  	<button type="submit" class="btn btn-primary">Add</button>
+					</form>
+					
+			</div>
+                
+            </div>
+        </div>
     </div>
-  );
+    );
+  }
+
 }
 
 export default App;
